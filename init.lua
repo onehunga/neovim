386349b7056e19610dfbc6b7152c785401b548c6
onehunga <julian@michels.hamburg>
2024-10-31 18:06:07 +0100

    neovide config

--- init.lua
+++ init.lua
@@ -8,3 +8,10 @@ require("config.lazy")
 require("lazy").setup("plugins")
 
 vim.cmd([[ colorscheme gruvbox ]])
+
+if vim.g.neovide then
+	vim.o.guifont = "Agave Nerd Font:h16" -- text below applies for VimScript
+	vim.opt.linespace = 0
+	vim.g.transparency = 0.9
+	vim.g.neovide_transparency = 0.8
+end
