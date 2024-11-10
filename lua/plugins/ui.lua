4abbce1bb858e250a0259baae18c387e85da7574
onehunga <julian@michels.hamburg>
2024-11-10 12:20:44 +0100

    nice ui
    added fidget and noice, but disabled noices lsp status

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,33 @@
+return {
+	{
+		"j-hui/fidget.nvim",
+		opts = {
+			notification = {
+				window = {
+					winblend = 0,
+				},
+			},
+		},
+	},
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			lsp = {
+				progress = {
+					enabled = false,
+				},
+			},
+		},
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			{
+
+				"rcarriga/nvim-notify",
+				opts = {
+					background_colour = "#000000",
+				},
+			},
+		},
+	},
+}
