cacb6bc1afa1e85e4bdf7279f418fab906da87de
onehunga <julian@michels.hamburg>
2025-03-22 08:58:20 +0100

    configure copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,4 +1,13 @@
 return {
-	'zbirenbaum/copilot.lua',
-	opts = {},
-};
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	config = function()
+		require("copilot").setup({
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+			},
+		})
+	end,
+}
