471c28526feb0ed406aa26029c00ff8fa902ec6f
onehunga <julian@michels.hamburg>
2025-04-28 22:13:08 +0200

    my old basic options

---
+++ lua/config/opts.lua
@@ -0,0 +1,14 @@
+local tab_size = 4
+
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.tabstop = tab_size
+vim.o.shiftwidth = tab_size
+vim.o.expandtab = false
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.clipboard = "unnamedplus"
+
+vim.o.laststatus = 3
+
+vim.g.rust_recommended_style = false
