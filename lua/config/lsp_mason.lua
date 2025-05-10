d48f928bc0c6d8b1109e67f989cb9d1e4ce2a307
onehunga <julian@michels.hamburg>
2025-05-10 09:37:36 +0200

    migrate mason-lspconfig

--- lua/config/lsp_mason.lua
+++ lua/config/lsp_mason.lua
@@ -1,34 +1,22 @@
-local mason = require 'mason'
-local mason_lspconfig = require 'mason-lspconfig'
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = {
+			runtime = {
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				globals = {
+					"vim",
+					"require",
+				},
+			},
+		},
+	},
+})
 
-mason.setup {}
-mason_lspconfig.setup {}
-
-mason_lspconfig.setup_handlers {
-	function(server_name)
-		local opts = {}
-
-		if server_name == 'lua_ls' then
-			opts = {
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { 'vim' }
-						},
-						workspace = {
-							checkThirdParty = false,
-							library = {
-								[vim.fn.expand '$VIMRUNTIME/lua'] = true,
-								[vim.fn.stdpath 'config' .. '/lua'] = true,
-								[vim.fn.stdpath 'data' .. '/lazy'] = true
-							}
-						}
-					}
-				}
-			}
-		end
-
-		require('lspconfig')[server_name].setup(opts)
-	end
-}
-
+local mason = require("mason")
+local mason_lspconfig = require("mason-lspconfig")
+mason.setup({})
+mason_lspconfig.setup({
+	ensure_installed = { "lua_ls" },
+})
--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,14 +1,23 @@
 return {
 	"stevearc/conform.nvim",
 	opts = {
+		notify_on_error = false,
+		formatters = {
+			odinfmt = {
+				command = "odinfmt",
+				args = { "-stdin" },
+				stdin = true,
+			},
+		},
 		format_on_save = {
-			lsp_format = 'first'
+			lsp_format = "first",
 		},
 		formatters_by_ft = {
 			lua = { "stylua" },
 			c = { "clang-format" },
 			cpp = { "clang-format" },
 			rust = { "rustfmt" },
+			odin = { "odinfmt" },
 		},
 	},
 }
